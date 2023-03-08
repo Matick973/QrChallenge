0a38527e07af14936500ffba1c4b15f7852b96bd
Matick973 <97759853+Matick973@users.noreply.github.com>
2023-03-08 02:24:04 +0100

    First commit
    React project QR code Challenge

---
+++ qr-code-component/src/App.js
@@ -0,0 +1,28 @@
+import logo from '../src/image/image-qr-code.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="card-container">
+
+      <div className="img-container">
+       
+      </div>
+
+      <div className="txt-container">
+        
+        <div className="card-title">
+          <h1>Improve your front-end skills by building projects</h1>
+        </div>
+
+        <div className="card-text">
+          <p>Scan the QR code to visit Frontend Mentor and take your coding skills to the next  level</p>
+        </div>
+
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
