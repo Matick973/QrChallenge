212e7636c0248a7db65c334b4ea349bc258dca65
Matick973 <97759853+Matick973@users.noreply.github.com>
2023-03-20 01:07:33 +0100

    Simplify directory acces
    cd

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from '../src/image/image-qr-code.png';
 import './App.css';
 
 function App() {
@@ -11,13 +10,13 @@ function App() {
 
       <div className="txt-container">
         
-        <div className="card-title">
-          <h1>Improve your front-end skills by building projects</h1>
-        </div>
+       
+          <h2>Improve your front-end skills by building projects</h2>
+    
 
-        <div className="card-text">
+   
           <p>Scan the QR code to visit Frontend Mentor and take your coding skills to the next  level</p>
-        </div>
+        
 
       </div>
 
